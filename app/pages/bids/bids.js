678020c31da95716780a338df5e622119c56f6cc
Victor Repkow <repkowvictor@gmail.com>
2016-02-20 21:45:23 -0500

    Added makeBid prompt

--- app/pages/bids/bids.js
+++ app/pages/bids/bids.js
@@ -1,4 +1,4 @@
-import {Page, NavController} from 'ionic-framework/ionic';
+import {Page, NavController, Alert} from 'ionic-framework/ionic';
 
 /*
   Generated class for the BidsPage page.
@@ -12,5 +12,30 @@ import {Page, NavController} from 'ionic-framework/ionic';
 export class BidsPage {
   constructor(nav: NavController) {
     this.nav = nav;
+  }
+
+  editBid(item) {
+    let prompt = Alert.create();
+    prompt.setTitle('Make a Bid');
+    prompt.setSubTitle('Please enter your best offer for: ' + item);
+    prompt.setMessage('You will automatically over-bid other bids until you reach your highest offer');
+    prompt.addInput({
+      type: 'number',
+      name: 'amount',
+      placeholder: 'Amount (current OBO: $10)'
+    });
+    prompt.addButton({
+      text: 'Cancel',
+      handler: data => {
+        console.log('Cancel clicked');
+      }
+    });
+    prompt.addButton({
+      text: 'Save',
+      handler: data => {
+        console.log('Saved clicked');
+      }
+    });
+    this.nav.present(prompt);
   }
 }
